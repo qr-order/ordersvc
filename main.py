ccb7f86c613972542eb9b76a29db4d896156b403
DongjunLim <lacuna7304@gmail.com>
2021-01-30 23:54:40 +0900

    remove runserver code

--- main.py
+++ main.py
@@ -7,7 +7,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, World"}
-
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
