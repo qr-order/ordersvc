45f6e6db6e041db1ef92a669c3aa0b9c6303fef5
Dongjun Joel Lim <lacuna7304@gmail.com>
2021-02-07 21:34:54 +0900

    Add Continuous Deployment pipeline (#3)
    * Create CD pipeline

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello, World"}
-
-
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
